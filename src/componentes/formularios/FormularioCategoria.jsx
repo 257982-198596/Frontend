9e0c1184624ca4f0cabfba671d1d4d66004ad18e
Sebastian Berrospe <sebastian.berrospe@zion.com.uy>
2024-12-20 09:14:16 -0300

    primeras pruebas vistas, form, slice y api

---
+++ src/componentes/formularios/FormularioCategoria.jsx
@@ -0,0 +1,52 @@
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+function FormularioCategoria({ formData, handleChange, onSubmit, modo }) {
+  const isReadOnly = modo === "detalle";
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="nombre">Nombre de la Categoría</Form.Label>
+        <Form.Control
+          type="text"
+          id="nombre"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+          placeholder="Ingrese el nombre de la categoría"
+          readOnly={isReadOnly}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="descripcion">Descripción</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          id="descripcion"
+          name="descripcion"
+          value={formData.descripcion}
+          onChange={handleChange}
+          placeholder="Ingrese la descripción de la categoría"
+          readOnly={isReadOnly}
+        />
+      </Form.Group>
+
+      <div className="d-flex justify-content-center">
+        {!isReadOnly && (
+          <Button variant="dark" type="submit" className="botones-formularios">
+            {modo === "alta" ? "Crear Categoría" : "Guardar Cambios"}
+          </Button>
+        )}
+        <Link to="/categorias">
+          <Button variant="secondary" className="botones-formularios">Volver al Listado</Button>
+        </Link>
+      </div>
+      <ToastContainer />
+    </Form>
+  );
+}
+
+export default FormularioCategoria;
--- src/paginas/Categorias.jsx
+++ src/paginas/Categorias.jsx
@@ -1,11 +1,119 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { FiList } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import EliminarCategoria from "../paginas/categorias/EliminarCategoria";
+import { borrarCategoriaEnAPI } from "../api/categoriaServicios";
+import { eliminarCategoria } from "../slices/sliceCategorias";
 
 function Categorias() {
-    return (
-        <div>
-            <h2>Categorías</h2>
-            <p>Contenido relacionado con categorias.</p>
-        </div>
-    );
+  const categorias = useSelector((state) => state.sliceCategorias.categorias);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [showModal, setShowModal] = useState(false);
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(null);
+
+  const verDetallesCategoria = (idCategoria) => {
+    navigate(`/categorias/detalle/${idCategoria}`);
+  };
+  const editarCategoria = (idCategoria) => {
+    navigate(`/categorias/editar/${idCategoria}`);
+  };
+
+  const handleAbrirModal = (idCategoria) => {
+    setCategoriaSeleccionada(idCategoria);
+    setShowModal(true);
+  };
+
+  const handleCerrarModal = () => {
+    setShowModal(false);
+    setCategoriaSeleccionada(null);
+  };
+
+  const borrarCategoria = async () => {
+    try {
+      console.log("Categoría seleccionada para eliminar:", categoriaSeleccionada);
+
+      const idCategoria = await borrarCategoriaEnAPI(categoriaSeleccionada);
+      const payload = { id: idCategoria };
+      dispatch(eliminarCategoria(payload));
+      handleCerrarModal();
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <div>
+      <FiList className="icono-seccion" />
+      <h2>Categorías</h2>
+      <Link to="/categorias/alta">
+        <button className="btn oblcolor">Nueva Categoría</button>
+      </Link>
+
+      <br></br>
+      <div className="espacio"></div>
+      <table className="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Descripción</th>
+            <th scope="col">Detalles</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categorias.map((categoria) => {
+            return (
+              <tr key={categoria.id}>
+                <td>{categoria.id}</td>
+                <td>{categoria.nombre}</td>
+                <td>{categoria.descripcion}</td>
+
+                <td>
+                  <button
+                    className="btn btn-danger oblcolor"
+                    onClick={() => verDetallesCategoria(categoria.id)}
+                  >
+                    Ver Más
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger oblcolor"
+                    onClick={() => editarCategoria(categoria.id)}
+                  >
+                    Editar
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger oblcolor"
+                    onClick={() => handleAbrirModal(categoria.id)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {/* Modal para Confirmar Eliminación */}
+      <EliminarCategoria
+        show={showModal}
+        handleClose={handleCerrarModal}
+        handleEliminar={borrarCategoria}
+        objAEliminar={"categoría"}
+      />
+    </div>
+  );
 }
 
 export default Categorias;
