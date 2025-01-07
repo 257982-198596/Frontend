0dea541fc9a0f8e67bf23edb7ba336bd5e3aa694
zionweb <92651245+zionweb@users.noreply.github.com>
2025-01-07 13:29:17 -0300

    Crecion de vistas para reporte VencimientosDelMes

---
+++ src/paginas/reportes/VencimientosDelMes.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { FaCalendarPlus } from "react-icons/fa";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import { GiReceiveMoney } from "react-icons/gi";
+import { TbPigMoney } from "react-icons/tb";
+import { obtenerServiciosVencenEsteMesAPI } from '../../api/servicioServiciosDelCliente';
+
+const VencimientosDelMes = () => {
+  const [serviciosVencenEsteMes, setServiciosVencenEsteMes] = useState([]);
+
+  useEffect(() => {
+    const cargarServiciosVencenEsteMes = async () => {
+      try {
+        const response = await obtenerServiciosVencenEsteMesAPI();
+        setServiciosVencenEsteMes(response.data);
+      } catch (error) {
+        console.error("Error al obtener los servicios que vencen este mes:", error);
+      }
+    };
+
+    cargarServiciosVencenEsteMes();
+  }, []);
+
+  return (
+    <div className="container">
+      <h3>Vencimientos de OCTUBREEEEE</h3>
+      <div className="row mb-3">
+        <div className="col-md-3 indicador my-1 mx-1">
+          <FaCalendarPlus className="icono-indicador" />
+          <h5>Cantidad Vencimientos</h5>
+          <p className="valor-indicador">12</p> 
+        </div>
+        <div className="col-md-3 indicador my-1 mx-1">
+          <FaMoneyBillTrendUp className="icono-indicador" />
+          <h5>Monto total de Renovaciones</h5>
+          <p className="valor-indicador-pequeno">$867</p> 
+        </div>
+        <div className="col-md-3 indicador my-1  mx-1">
+          <GiReceiveMoney className="icono-indicador" />
+          <h5>Monto ya cobrado</h5>
+          <p className="valor-indicador">$45</p> 
+        </div>
+        <div className="col-md-3 indicador my-1  mx-1">
+          <TbPigMoney className="icono-indicador" />
+          <h5>Monto pendiente de Cobro</h5>
+          <p className="valor-indicador">$756</p> 
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          <h5>Monto por Cobrar por Categoría de Servicio</h5>
+          <div className="progress mb-2">
+            <div className="progress-bar bg-info" role="progressbar" style={{width: '30%'}} aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">Servicio A - $300</div>
+          </div>
+          <div className="progress mb-2">
+            <div className="progress-bar bg-success" role="progressbar" style={{width: '50%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">Servicio B - $500</div>
+          </div>
+          <div className="progress">
+            <div className="progress-bar bg-warning" role="progressbar" style={{width: '20%'}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">Servicio C - $200</div>
+          </div>
+        </div>
+        
+      </div>
+      <div className="row mt-4">
+        <div className="col-md-12">
+          <h5>Servicios que Vencen en el Mes Corriente</h5>
+          {Array.isArray(serviciosVencenEsteMes) && serviciosVencenEsteMes.length > 0 ? (
+            <table className="table table-striped table-dark">
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Servicio</th>
+                <th scope="col">Monto</th>
+                <th scope="col">Moneda</th>
+                <th scope="col">Frecuencia</th>
+                <th scope="col">Fecha Vencimiento</th>
+                <th scope="col">Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {serviciosVencenEsteMes.map((servicio) => {
+           
+                return (
+                  <tr key={servicio.id}>
+                    <td>{servicio.id}</td>
+                    <td>{servicio.fechaEnvio}</td>
+                    <td>{servicio.clienteNotificado.nombre}</td>
+                    <td>{servicio.mensaje}</td>
+                    <td>{servicio.estadoDeNotificacion.nombre}</td>
+                    <td>{servicio.servicioNotificado.descripcion}</td>
+                    
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          ) : (
+            <p>No hay servicios que venzan este mes.</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VencimientosDelMes;
