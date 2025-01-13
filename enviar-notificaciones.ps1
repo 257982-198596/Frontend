2040e04176d082b693290450704d7ef35a4c0c08
zionweb <92651245+zionweb@users.noreply.github.com>
2025-01-13 18:03:07 -0300

    correcciones en script

--- enviar-notificaciones.ps1
+++ enviar-notificaciones.ps1
@@ -1,4 +1,4 @@
-$apiUrl = "http://localhost:18190/api/notificaciones/procesar-vencimientos"
+$apiUrl = "https://backend-proyecto-integrador-257982-198596-edhucddfd5axfwhe.eastus-01.azurewebsites.net/api/notificaciones/procesar-vencimientos"
 $headers = @{
     "Content-Type" = "application/json; charset=UTF-8"
 }
