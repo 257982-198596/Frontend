7fd1dbdb87fac2f2a34f5a415a9e0565b1d28a23
zionweb <92651245+zionweb@users.noreply.github.com>
2025-01-22 19:52:15 -0300

    agregado de sidebarcliente y footer

--- src/paginas/areaClientes/HomeClientes.jsx
+++ src/paginas/areaClientes/HomeClientes.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from '../../componentes/Header';
+import SidebarClientes from '../../componentes/SidebarClientes'; // Import SidebarClientes
+import Footer from '../../componentes/Footer'; // Import Footer
+import { Outlet } from 'react-router-dom'; // Import Outlet
 
 export default function HomeClientes() {
   const nombreCliente = localStorage.getItem('nombreCliente');
@@ -7,7 +10,11 @@ export default function HomeClientes() {
   return (
     <>
       <Header nombreCliente={nombreCliente} />
-      <div>HomeClientes</div>
+      <SidebarClientes /> 
+      <div>
+        <Outlet /> 
+      </div>
+      <Footer /> 
     </>
   );
 }
